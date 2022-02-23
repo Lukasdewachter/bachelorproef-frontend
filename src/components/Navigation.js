75a3492f2d137af59d0b0c9a036c4c3b38a46fca
lukas <lukas.dewachter@student.kuleuven.be>
2022-02-23 16:44:48 +0100

    navbar klaar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './Navigation.css'
 
 function Navigation() {
   return (
     <div className="navigation">
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
+      <nav className="navbar navbar-expand navbar-dark">
         <div className="container">
           <NavLink className="navbar-brand" to="/">
             Masterproef tool
