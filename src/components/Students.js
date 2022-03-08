fee0a1f4cfc28ab4378b464698fd1a2229e0e417
lukas <lukas.dewachter@student.kuleuven.be>
2022-03-08 14:02:58 +0100

    Handlechange 1 functie

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
-import{Component} from 'react'
+import { Component } from 'react'
 import './MainStyleSheet.css'
 
 const api = axios.create({
     baseURL: `http://localhost:8080/student/`
 })
-class Students extends Component{
-    constructor(){
+class Students extends Component {
+    constructor() {
         super();
         this.getStudent = this.getStudent.bind(this);
         this.addStudent = this.addStudent.bind(this);
@@ -18,12 +18,11 @@ class Students extends Component{
         studenten: [] //mag niet met hoofdletter
     }
     getStudent = async () => {
-        let data  = await api.get('/all').then(({data}) => data);
-        this.setState({studenten: data})
+        let data = await api.get('/all').then(({ data }) => data);
+        this.setState({ studenten: data })
     }
-
     addStudent = event => {
-        api.post('/add',{
+        api.post('/add', {
             name: this.state.name,
             surname: this.state.surname,
             tel: this.state.tel,
@@ -31,136 +30,118 @@ class Students extends Component{
             fieldOfStudy: this.state.fieldOfStudy,
             mail: this.state.mail,
             campus: this.state.campus
-          });
+        });
         this.getStudent();
     }
-    handleNameChange = event =>{
-        this.setState({
-            name: event.target.value,
+    handleAddChange = (event) =>{
+        const [addData, setData] = useState({
+            name:'',
+            surname:'',
+            tel:'',
+            address:'',
+            fieldOfStudy:'',
+            mail:'',
+            campus:''
         })
+        event.preventdefault();
+        const fieldName = event.target.getAttribute('name');
+        const fieldValue = event.target.value;
+        const newData = {...addData};
+        newData[fieldName] = fieldValue;
+        setData(newData);
     }
-    handleSurNameChange = event =>{
-        this.setState({
-            surname: event.target.value,
-        })
-    }
-    handleTelChange = event =>{
-        this.setState({
-            tel: event.target.value,
-        })
-    }
-    handleAddressChange = event =>{
-        this.setState({
-            address: event.target.value,
-        })
-    }
-    handleFieldOfStudyChange = event =>{
-        this.setState({
-            fieldOfStudy: event.target.value,
-        })
-    }
-    handleMailChange = event =>{
-        this.setState({
-            mail: event.target.value,
-        })
-    }
-    handleCampusChange = event =>{
-        this.setState({
-            campus: event.target.value,
-        })
-    }
-    deleteStudent = ({idStudent},event) => {
+    deleteStudent = ({ idStudent }, event) => {
         api.delete(`/delete?idStudent=${idStudent}`);
         this.getStudent();
     }
-    render(){
+    render() {
         return (
             <div className="Students">
                 <table>
                     <thead>
-                    <tr>
-                        <th>Student id</th>
-                        <th>Name</th>
-                        <th>Surname</th>
-                        <th>Telephone Number</th>
-                        <th>Address</th>
-                        <th>Field of Study</th>
-                        <th>Mail</th>
-                        <th>Campus</th>
-                        <th>Edit/Delete</th>
-                    </tr>
+                        <tr>
+                            <th>Student id</th>
+                            <th>Name</th>
+                            <th>Surname</th>
+                            <th>Telephone Number</th>
+                            <th>Address</th>
+                            <th>Field of Study</th>
+                            <th>Mail</th>
+                            <th>Campus</th>
+                            <th>Edit/Delete</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {this.state.studenten.map(student => (
-                        <tr key={'student_'+student.idStudent+student.name+student.surname+student.mail+student.tel+student.address+student.fieldOfStudy+student.campus}>
-                            <td>{student.idStudent}</td>
-                            <td>{student.name}</td>
-                            <td>{student.surname}</td>
-                            <td>{student.tel}</td>
-                            <td>{student.address}</td>
-                            <td>{student.fieldOfStudy}</td>
-                            <td>{student.mail}</td>
-                            <td>{student.campus}</td>
-                            <td><button onClick={()=>this.deleteStudent(student.idStudent)}>x</button></td>
-                        </tr>
-                    ))}
+                        {this.state.studenten.map(student => (
+                            <tr key={'student_' + student.idStudent + student.name + student.surname + student.mail + student.tel + student.address + student.fieldOfStudy + student.campus}>
+                                <td>{student.idStudent}</td>
+                                <td>{student.name}</td>
+                                <td>{student.surname}</td>
+                                <td>{student.tel}</td>
+                                <td>{student.address}</td>
+                                <td>{student.fieldOfStudy}</td>
+                                <td>{student.mail}</td>
+                                <td>{student.campus}</td>
+                                <td><button onClick={() => this.deleteStudent(student.idStudent)}>x</button></td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
                 <form onSubmit={this.addStudent}>
                     <label>New Student</label>
-                    <input 
+                    <input
                         type='text'
                         name='name'
                         required='required'
                         placeholder='name'
-                        onChange=  {this.handleNameChange}
+                        onChange={this.handleAddChange}
                     />
-                    <input 
+                    <input
                         type='text'
                         name='surname'
                         required='required'
                         placeholder='surname'
-                        onChange=  {this.handleSurNameChange}
+                    onChange={this.handleAddChange}
                     />
-                    <input 
+                    <input
                         type='number'
                         name='tel'
                         required='required'
                         placeholder='tel number'
-                        onChange=  {this.handleTelChange}
+                        onChange={this.handleAddChange}
                     />
-                    <input 
+                    <input
                         type='text'
                         name='address'
                         required='required'
                         placeholder='address'
-                        onChange=  {this.handleAddressChange}
+                        onChange={this.handleAddChange}
                     />
-                    <input 
+                    <input
                         type='text'
                         name='fieldOfStudy'
                         required='required'
                         placeholder='field of study'
-                        onChange=  {this.handleFieldOfStudyChange}
+                        onChange={this.handleAddChange}
                     />
-                    <input 
+                    <input
                         type='email'
                         name='mail'
                         required='required'
                         placeholder='mail'
-                        onChange=  {this.handleMailChange}
+                        onChange={this.handleAddChange}
                     />
-                    <input 
+                    <input
                         type='text'
                         name='campus'
                         required='required'
                         placeholder='campus'
-                        onChange=  {this.handleCampusChange}
+                        onChange={this.handleAddChange}
                     />
-                    <input type="submit" value="add student"/>
+                    <input type="submit" value="add student" />
                 </form>
             </div>
-            
+
         );
     }
 }
