89dbef329cbf4bbba00069848435fa840dbef490
Wannes Van de Putte <vandeputte.wannes@gmail.com>
2022-05-09 22:25:19 +0200

    Footer & Header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,16 +2,14 @@ import React from "react";
 import './Footer.css'
 function Footer() {
   return (
-    <div className="footer">
-      <footer className="py-5 fixed-bottom">
+      <footer className="footer py-5 fixed-bottom">
         <div className="container">
           <p className="m-0 text-center text-white">
             KULeuven
           </p>
         </div>
       </footer>
-    </div>
-  );
+    );
 }
 
 export default Footer;
