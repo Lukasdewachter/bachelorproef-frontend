f53c2f3147e8141a8feef32e707378b944a300f1
Lukasdewachter <99643379+Lukasdewachter@users.noreply.github.com>
2022-05-15 17:36:26 +0200

    Fix professor & company (#11)
    Co-authored-by: Wannes Van de Putte <vandeputte.wannes@gmail.com>

--- src/components/Thesis/ThesisProfComp.js
+++ src/components/Thesis/ThesisProfComp.js
@@ -1,9 +1,9 @@
 import React from "react";
-import '../MainStyleSheet.css'
+import './ThesisStyleSheet.css'
 
-const ProfComp = ({thesis,handleMoreInfoClick }) =>{
+const ProfComp = ({thesis, handleMoreInfoClick }) =>{
     return(
-        <div>
+        <div  className="thesisBlock">
             <h2>{thesis.name}</h2>
             <p>{thesis.shortDescription}</p>
             <p><ion-icon name="location-outline"></ion-icon> {thesis.campus}</p>
@@ -15,4 +15,33 @@ const ProfComp = ({thesis,handleMoreInfoClick }) =>{
         </div>
         );
 };
-export {ProfComp};
+
+
+const ThesisInfo = ({thesis, handleMoreInfoClick, handleStarClick, handleBookmarkClick}) => {
+    console.log(thesis.bookmarked)
+    return(
+        <div className="thesisInfoBlock">
+            <ion-button onClick={(event)=>handleMoreInfoClick(event, null)}><ion-icon name="close" size="large" class="thesisInfoClose"  title="Sluit meer info"></ion-icon></ion-button>
+            <h1>{thesis.name}</h1>
+            <p>{thesis.longDescription}</p>
+            <p><ion-icon name="location-outline"></ion-icon> {thesis.campus}</p>
+            <p><ion-icon name="book"></ion-icon> {thesis.fieldOfStudy}</p>
+            <p><ion-icon name="school"></ion-icon> {thesis.promotor}</p>
+            <p><ion-icon name="people"></ion-icon> {thesis.numberOfPers}</p>
+            <div className="starSelect">
+                <ion-button onClick={(event)=>handleStarClick(event, thesis.id, 1)}><ion-icon name="star" size="large" class="star"></ion-icon></ion-button>
+                <ion-button onClick={(event)=>handleStarClick(event, thesis.id, 2)}><ion-icon name="star" size="large" class="star"></ion-icon></ion-button>
+                <ion-button onClick={(event)=>handleStarClick(event, thesis.id, 3)}><ion-icon name="star" size="large" class="star"></ion-icon></ion-button>
+            </div>
+            <div className="bookmark">
+                {!thesis.bookmarked && (
+                    <ion-button onClick={(event)=>handleBookmarkClick(event, thesis.id)}><ion-icon name="bookmark-outline" size="large"></ion-icon></ion-button>
+                )}
+                {thesis.bookmarked && (
+                    <ion-button onClick={(event)=>handleBookmarkClick(event, thesis.id)}><ion-icon name="bookmark" size="large"></ion-icon></ion-button>
+                )}
+            </div>
+        </div>
+    )
+}
+export {ProfComp, ThesisInfo};
