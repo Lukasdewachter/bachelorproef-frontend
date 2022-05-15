eb44fd6ddb85ef0a97ef51387deb2930b62353dd
Wannes Van de Putte <vandeputte.wannes@gmail.com>
2022-05-15 23:11:38 +0200

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,17 @@ import './Footer.css'
 function Footer() {
   return (
       <footer className="footer">
-        <div className="container">
-          <p className="m-0 text-center text-white">
-            KULeuven
+          <p className="text-left text-white">
+            2022 © KU Leuven
           </p>
-        </div>
+
+          <div className="socials">
+            <a href="https://www.facebook.com/KULeuven/" target="_blank"><ion-button onClick=""><ion-icon name="logo-facebook" size="large"></ion-icon> </ion-button></a>
+            <a href="https://twitter.com/KU_Leuven/" target="_blank"><ion-button onClick=""><ion-icon name="logo-twitter" size="large"></ion-icon> </ion-button></a>
+            <a href="https://www.instagram.com/kuleuven/" target="_blank"><ion-button onClick=""><ion-icon name="logo-instagram" size="large"></ion-icon> </ion-button></a>
+            <a href="https://www.linkedin.com/school/ku_leuven/" target="_blank"><ion-button onClick=""><ion-icon name="logo-linkedin" size="large"></ion-icon> </ion-button></a>
+            <a href="https://www.youtube.com/user/kuleuven/" target="_blank"><ion-button onClick=""><ion-icon name="logo-youtube" size="large"></ion-icon> </ion-button></a>
+          </div>
       </footer>
     );
 }
